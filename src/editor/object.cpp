d7e7c6fb8e80e2465d5d9ade0fd681cca2160ba3
ktf <not.discostoo@gmail.com>
2005-03-16 05:48:36 +0000

    objects class now exists/rules

--- src/editor/object.cpp
+++ src/editor/object.cpp
@@ -1,10 +1,12 @@
 #include <SDL/SDL.h>
 #include "object.h"
+#include "level.h"
 #include "editor.h"
 
 
-// init the surface
+// init the surfaces
 SDL_Surface * Object::psdlsObjects = NULL;
+SDL_Surface * Object::psdlsPlayers = NULL;
 
 
 Object::Object(int x, int y)
@@ -17,24 +19,107 @@ Object::Object(int x, int y)
     if(!psdlsObjects)
       QuitWithError("Error creating surface for objects.bmp resource!\n");
   }
+  if(!psdlsPlayers)
+  {
+    psdlsPlayers = SDL_LoadBMP(LoadResource("players.bmp", RESOURCE_GRAPHIC));
+    if(!psdlsPlayers)
+      QuitWithError("Error creating surface for players.bmp resource!\n");
+  }
 }
 
 bool Object::BlitToSurface(SDL_Surface * psdlsDest)
 {
+  if(SDL_BlitSurface(psdlsObjects, &rcSource, psdlsDest, &rcDest) < 0)
+    return false;
+
   return true;
 }
 
+char Object::GetChar()
+{
+  return byObjChar;
+}
 
-/////////////////////
-//    WALL JUNK    /
-///////////////////
+
+//////////////////////////////////
+//  ALL THE INHERITING OBJECTS  /
+////////////////////////////////
 Wall::Wall(int x, int y)
  :Object(x, y)
 {
   SetRect(&rcSource, 0, 0, 10, 10);
+  byObjChar = CHAR_WALL;
+}
+Invulnerability::Invulnerability(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 10, 0, 10, 10);
+  byObjChar = CHAR_INVULNERABILITY;
+}
+Health::Health(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 40, 0, 10, 10);
+  byObjChar = CHAR_HEALTH;
+}
+Powup::Powup(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 20, 0, 10, 10);
+  byObjChar = CHAR_POWUP;
+}
+Powdown::Powdown(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 30, 0, 10, 10);
+  byObjChar = CHAR_POWDOWN;
+}
+Bomn::Bomn(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 70, 0, 10, 10);
+  byObjChar = CHAR_BOMN;
+}
+Warp::Warp(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 80, 0, 10, 10);
+  byObjChar = CHAR_WARP;
 }
 
-//bool Wall::BlitToSurface(SDL_Surface * psdlsDest)
-//{
-//  return true;
-//}
+P1start::P1start(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 0, 0, 10, 10);
+  byObjChar = CHAR_P1START;
+}
+bool P1start::BlitToSurface(SDL_Surface * psdlsDest)
+{
+ if(SDL_BlitSurface(psdlsPlayers, &rcSource, psdlsDest, &rcDest) < 0)
+    return false;
+  return true;
+}
+
+P2start::P2start(int x, int y)
+ :Object(x, y)
+{
+  SetRect(&rcSource, 10, 0, 10, 10);
+  byObjChar = CHAR_P2START;
+}
+bool P2start::BlitToSurface(SDL_Surface * psdlsDest)
+{
+ if(SDL_BlitSurface(psdlsPlayers, &rcSource, psdlsDest, &rcDest) < 0)
+    return false;
+  return true;
+}
+
+None::None(int x, int y)
+ :Object(x, y)
+{
+//  SetRect(&rcSource, 0, 0, 10, 10);
+  byObjChar = CHAR_NONE;
+}
+bool None::BlitToSurface(SDL_Surface * psdlsDest)
+{
+  return true;
+}
--- src/editor/object.h
+++ src/editor/object.h
@@ -10,11 +10,14 @@ class Object
 
 //    virtual bool BlitToSurface(SDL_Surface *) = 0;
     bool BlitToSurface(SDL_Surface *);
+    char GetChar();
 
   protected:
     static SDL_Surface * psdlsObjects; // I think this means all the objects share this (it'd better mean that...)
+    static SDL_Surface * psdlsPlayers; // need this for the P1start and P2start graphics
     SDL_Rect rcSource;
     SDL_Rect rcDest;
+    char byObjChar;
 };
 
 
@@ -23,7 +26,54 @@ class Wall : public Object
 {
   public:
     Wall(int, int);
-//    bool BlitToSurface(SDL_Surface *);
+};
+class Invulnerability : public Object
+{
+  public:
+    Invulnerability(int, int);
+};
+class Health : public Object
+{
+  public:
+    Health(int, int);
+};
+class Powup : public Object
+{
+  public:
+    Powup(int, int);
+};
+class Powdown : public Object
+{
+  public:
+    Powdown(int, int);
+};
+class Bomn : public Object
+{
+  public:
+    Bomn(int, int);
+};
+class Warp : public Object
+{
+  public:
+    Warp(int, int);
+};
+class P1start : public Object
+{
+  public:
+    P1start(int, int);
+    bool BlitToSurface(SDL_Surface *); // need this 'cause it uses a different surface
+};
+class P2start : public Object
+{
+  public:
+    P2start(int, int);
+    bool BlitToSurface(SDL_Surface *); // need this 'cause it uses a different surface
+};
+class None : public Object
+{
+  public:
+    None(int, int);
+    bool BlitToSurface(SDL_Surface *); // this needs its own 'cause it won't actually DO anything (to save time)
 };
 
 
